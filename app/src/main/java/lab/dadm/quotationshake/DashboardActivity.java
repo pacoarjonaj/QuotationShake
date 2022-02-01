11e3e3b11d2a7e0f7a9349c243afc2045cda31e8
pacoarjonaj <pacoarjonaj@users.noreply.github.com>
2022-02-01 16:31:26 +0100

    lab sesion

---
+++ app/src/main/java/lab/dadm/quotationshake/DashboardActivity.java
@@ -0,0 +1,19 @@
+package lab.dadm.quotationshake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        Button buttonGetQuo = findViewById(R.id.buttonGetQuo);
+        //OnClickListener
+
+    }
+}
