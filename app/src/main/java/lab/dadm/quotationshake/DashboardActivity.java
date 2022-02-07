3c41e5b1425d8e664ae8f17edd1c879e731d6c6c
pacoarjonaj <pacoarjonaj@users.noreply.github.com>
2022-02-07 17:57:36 +0100

    new activities + about page done

--- app/src/main/java/lab/dadm/quotationshake/DashboardActivity.java
+++ app/src/main/java/lab/dadm/quotationshake/DashboardActivity.java
@@ -2,6 +2,7 @@ package lab.dadm.quotationshake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -12,8 +13,30 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        Button buttonGetQuo = findViewById(R.id.buttonGetQuo);
-        //OnClickListener
+        final Button bGetQuo = findViewById(R.id.buttonGetQuo);
+        bGetQuo.setOnClickListener(v -> {
+            final Intent intent = new Intent(DashboardActivity.this, QuotationActivity.class);
+            startActivity(intent);
+        });
+
+        final Button bFavQuo = findViewById(R.id.buttonFavQuo);
+        bFavQuo.setOnClickListener(v -> {
+            final Intent intent = new Intent(DashboardActivity.this, FavouriteActivity.class);
+            startActivity(intent);
+        });
+
+        final Button bSett = findViewById(R.id.buttonSettings);
+        bSett.setOnClickListener(v -> {
+            final Intent intent = new Intent(DashboardActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        });
+
+        final Button bAbout = findViewById(R.id.buttonAbout);
+        bAbout.setOnClickListener(v -> {
+            final Intent intent = new Intent(DashboardActivity.this, AboutActivity.class);
+            startActivity(intent);
+        });
+
 
     }
 }
