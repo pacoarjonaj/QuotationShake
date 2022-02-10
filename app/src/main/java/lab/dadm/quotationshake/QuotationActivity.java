2818c699889f2f9b40f0fa7b0db52c5eb2fd7d00
pacoarjonaj <pacoarjonaj@users.noreply.github.com>
2022-02-10 13:51:45 +0100

    rotation, quotation and translation done.  to fix strings values and imageButton listener

--- app/src/main/java/lab/dadm/quotationshake/QuotationActivity.java
+++ app/src/main/java/lab/dadm/quotationshake/QuotationActivity.java
@@ -2,7 +2,9 @@ package lab.dadm.quotationshake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class QuotationActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class QuotationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotation);
+
+        // Asociar el nombre al TextView
+
+        final ImageButton ibQuo = findViewById(R.id.imageButton);
+        ibQuo.setOnClickListener(v -> {
+            // Modificar Strings
+        });
     }
 }
